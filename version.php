ba42dcc9996d6aca509a5526cb7a515a2045dcff
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-03-20 17:03:09 +0200

    Add maturity to version.php

--- version.php
+++ version.php
@@ -2,3 +2,4 @@
 $plugin->component = 'block_vmchecker';
 $plugin->version = 2021102803;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->maturity = MATURITY_ALPHA;
