f4fda0261004fd6bfdb7d33ed6d80508b58d48c7
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-04-12 16:32:44 +0300

    Clean observer.php

--- classes/listener/observer.php
+++ classes/listener/observer.php
@@ -40,37 +40,36 @@ class observer {
         global $DB;
 
         $data = $event->get_data();
-        $submission = $DB->get_record($data['objecttable'],
+        $submission_file = $DB->get_record($data['objecttable'],
             array('id' => $data['objectid']));
 
         $vmchecker_options = $DB->get_record('block_vmchecker_options',
-            array('assignid' => $submission->assignment));
+            array('assignid' => $submission_file->assignment));
 
-            // The assignment is not a vmchecker type
+        // The assignment is not a vmchecker type
         if ($vmchecker_options == null)
             return;
 
-        if (!\block_vmchecker\listener\observer::stop_previous_attempt($submission->assignment))
+        if (!\block_vmchecker\listener\observer::stop_previous_attempt($submission_file->assignment))
             return;
 
         $config_data = $DB->get_record('block_instances',
             array('id' => $vmchecker_options->blockinstanceid), 'configdata')->configdata;
         $config = unserialize(base64_decode($config_data));
 
-        $cm = get_coursemodule_from_instance('assign', $submission->assignment, 0, false, MUST_EXIST);
+        $cm = get_coursemodule_from_instance('assign', $submission_file->assignment, 0, false, MUST_EXIST);
         $context = \context_module::instance($cm->id);
 
         $assign = new \assign($context, null, null);
-        $mocked_submission = new stdClass();
-        $mocked_submission->id = $submission->submission;
-        $submited_files = (new \assign_submission_file($assign, null))->get_files($mocked_submission, new stdClass);
+
+        $params = array('assignment'=>$assign->get_instance()->id, 'id'=>$submission_file->submission);
+        $user_submission = $DB->get_record('assign_submission', $params, '*', MUST_EXIST);
+
+        $submited_files = (new \assign_submission_file($assign, null))->get_files($user_submission, new stdClass);
         if (count($submited_files) !== 1)
             return;
 
         $submited_file = $submited_files[array_keys($submited_files)[0]];
-
-        $params = array('assignment'=>$assign->get_instance()->id, 'id'=>$submission->submission);
-        $user_submission = $DB->get_record('assign_submission', $params, '*', MUST_EXIST);
 
         $payload = array(
             'gitlab_private_token' => $config->gitlab_private_token,
@@ -87,7 +86,7 @@ class observer {
         $DB->insert_record('block_vmchecker_submissions',
             array(
                 'userid' => $user_submission->userid,
-                'assignid' => $submission->assignment,
+                'assignid' => $user_submission->assignment,
                 'uuid' => $response['UUID'],
                 'autograde' => $config->autograding === '1',
                 'updatedat' => time(),
