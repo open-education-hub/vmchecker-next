b73f7fcd4471941cfe55d1c8348879b911cb15e9
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-09-17 13:18:25 +0200

    Grooming for Moodle plugin publishing

---
+++ README.md
@@ -0,0 +1,7 @@
+# VMChecker Next
+
+VMChecker Next is a block type plugin. It is a solution developed for programming assignments,
+allowing teachers to create automatic checkers.
+
+User documentation available at: https://github.com/Jokeswar/vmchecker-next/wiki.
+Issues can be opened at: https://github.com/Jokeswar/vmchecker-next/issues.
--- version.php
+++ version.php
@@ -1,5 +1,6 @@
 <?php
 $plugin->component = 'block_vmchecker';
-$plugin->version = 2021102808;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2022091713;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '1.0.0';
