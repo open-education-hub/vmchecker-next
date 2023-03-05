908e85915ad3654950b798d8e6252ba35f24c114
Mihai Baruta <baruta.mihai99@gmail.com>
2023-03-05 16:20:49 +0200

    Remove hard-coded english lang strings
    Fixes #3

--- settings.php
+++ settings.php
@@ -27,14 +27,14 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext(
         'block_vmchecker/backend',
-        'vmck backend',
+        get_string('backend_title', 'block_vmchecker'),
         get_string('backend_description', 'block_vmchecker'),
         'http://localhost:8000/api/v1/',
         PARAM_RAW
     ));
     $settings->add(new admin_setting_configtext(
         'block_vmchecker/submission_check',
-        'Submissions check count',
+        get_string('check_count_title', 'block_vmchecker'),
         get_string('check_count_description', 'block_vmchecker'),
         50,
         PARAM_INT
